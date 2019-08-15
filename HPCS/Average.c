e7059409864734d3e5c5cc6b14f953fdba08cb1a
Shraddha Naik <53944286+shraddhanaik15@users.noreply.github.com>
2019-08-15 16:04:00 +0530

    Updated

--- HPCS/Average.c
+++ HPCS/Average.c
@@ -4,40 +4,36 @@
 
 int main(int argc, char *argv[])
 {
-	int rank, size,n,i,m,a[100],sum=0,average,j;
+	int rank, size,n,i,m,a[100],sum=0,average,j, rootaverage;
 	MPI_Init(&argc, &argv);
 	MPI_Status status;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
 	if(rank == 0)
 	{
-		printf("Enter no of Processes N\n");
-		scanf("%d", &n);
 		printf("Enter the value M\n");
 		scanf("%d", &m);
 		printf("Enter the M elements\n");
-		for(i=0;i<n*m;i++)
+		for(i=0;i<m*size;i++)
 			scanf("%d", &a[i]);
 	}
-	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&a, m ,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Bcast(&a, m/size ,MPI_INT,0,MPI_COMM_WORLD);
 
-		for(i=0;i<n;i++)
+		for(i=0;i<m/size;i++)
 	{
-		for(j=0;j<m;j++)
-		printf("Process %d has values %d\n", rank, a[j]);
+		sum = sum + a[i];
+		average = (float)sum/m;
 	}
-	
-	// for(i=0;i<n*m;i++)
-	// {
-	// 		sum = sum+a[j];
-	// 	}
-	// }
-	// printf("In process %d, Average is %d\n", rank,average);
+	printf("Process %d has sum %d\n", rank,sum);
+	printf("Process %d has average %d\n", rank,average);
 
-
-
+	MPI_Reduce(&average, &rootaverage,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+	if(rank == 0)
+	{
+		printf("Total average is %fin process %d\n",(float)rootaverage/m ,rank);
+	}
 	MPI_Finalize();
-
 }
+
