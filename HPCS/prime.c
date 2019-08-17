9f6e408efd1a00a4e99b90818937119ab67931b1
Shraddha Naik <53944286+shraddhanaik15@users.noreply.github.com>
2019-08-17 08:52:29 +0530

    Week - 2,Program - 3

--- HPCS/prime.c
+++ HPCS/prime.c
@@ -5,10 +5,8 @@ int main(int argc,char *argv[])
 	int size,rank,i,j,a[100]={0} ;
 	MPI_Status status;
 	MPI_Init(&argc,&argv);
-
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
-
 	if (rank==0)
 	{
 		for(i=2;i<=50;i++)
@@ -43,8 +41,6 @@ int main(int argc,char *argv[])
 				} 
 		}
 		}
-
 	MPI_Finalize();
 	return 1;
-
 }
