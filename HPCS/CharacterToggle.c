048eb0cce454e924e11c6f9d8fe531f9fc56323e
Shraddha Naik <53944286+shraddhanaik15@users.noreply.github.com>
2019-08-17 08:49:27 +0530

    Week - 1, Program - 2

--- HPCS/CharacterToggle.c
+++ HPCS/CharacterToggle.c
@@ -10,8 +10,6 @@ void main(int argc, char *argv[])
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-
 	if(rank==0)
 	{
 		printf("Enter the string\n");
@@ -42,7 +40,5 @@ void main(int argc, char *argv[])
 		}
 		MPI_Ssend(&b[0], n, MPI_CHAR, 0, 2, MPI_COMM_WORLD);
 	}
-	
 		MPI_Finalize();
-
 }
