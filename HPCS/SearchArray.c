3375af0b26eec87a85d570093d6936f6c5114d3a
Shraddha Naik <53944286+shraddhanaik15@users.noreply.github.com>
2019-08-17 08:51:42 +0530

    Week - 2,Program - 2

--- HPCS/SearchArray.c
+++ HPCS/SearchArray.c
@@ -8,7 +8,6 @@ void main(int argc, char *argv[])
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
 	if(rank == 0)
 	{
 		printf("Enter the no of elements in the array\n");
